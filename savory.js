22bce47bec9dd67212f3245c8b5172052ad44bf8
gini <gini.mcgee@gmail.com>
2013-08-16 01:47:12 -0700

    Adding checks to make sure fields are populated; styling

--- savory.js
+++ savory.js
@@ -10,10 +10,20 @@ $('body').delegate('.delete_field', 'click', function(){
 
 // Create Recipe
 $("#create_recipe a").on('click', function() {
-	$("#recipe_title").text($("#recipe_name").val());
-	$("#prepare").text($("#preparation").val());
-	$("#cook").text($("#cooking").val());
-	$("#serve").text($("#serving").val());
+	$("#recipe_title").html("<h2>"+($("#recipe_name").val())+"</h2>");
+
+	if($("#preparation").val()) {
+		$("#prepare").prepend("<h3>Preparation</h3>");
+		$("#prepare p").text($("#preparation").val());
+	}
+	if($("#cooking").val()) {
+		$("#cook").prepend("<h3>Cooking Instructions</h3>");
+		$("#cook p").text($("#cooking").val());
+	}
+	if($("#serving").val()) {
+		$("#serve").prepend("<h3>Serving Notes</h3>");
+		$("#serve p").text($("#serving").val());
+	}
 
 	$(".ingredient input").each(function() {
 		listIngredient = $(this).val();
